fcf19e4765f6fcf98e1c6c6eb8d18dbdaf2e9b34
Senait Mekonen Gebreegziabher <senimoke4@gmail.com>
2025-08-03 02:12:39 +0300

    new

---
+++ list.py
@@ -0,0 +1,26 @@
+# creating
+my_list = []
+
+# append
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# insert
+my_list.insert(1, 15)
+
+# extend
+my_list.extend([50, 60, 70])
+
+# remove
+my_list.pop()
+
+# sort
+my_list.sort()
+
+# find
+index_30 = my_list.index(30)
+
+# print
+print("The index of 30 is:", index_30)
