8b03f2450cfa40a0735cac5eaa41d72399214928
Senait Mekonen Gebreegziabher <senimoke4@gmail.com>
2025-08-03 01:34:11 +0300

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,25 @@
+# numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#  operation
+operation = input("Enter an operation (+, -, *, /): ")
+
+# performing
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Invalid operation. Please use +, -, *, or /.")
